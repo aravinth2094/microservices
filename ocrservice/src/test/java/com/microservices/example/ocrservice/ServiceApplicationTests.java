fd9676cde5ca649c769ec2b55412e5c564812bd2
Aravinth Sundaram <gosh.aravind@gmail.com>
2020-05-07 11:38:03 +0530

    OCRService Test cases initial commit

--- ocrservice/src/test/java/com/microservices/example/ocrservice/ServiceApplicationTests.java
+++ ocrservice/src/test/java/com/microservices/example/ocrservice/ServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.microservices.example.ocrservice;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	protected void testUserInfoForbidden() throws Exception {
+		mockMvc.perform(get("/userinfo")).andExpect(status().isUnauthorized());
 	}
 
 }
